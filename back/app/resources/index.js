b9da0011e6de4d37cee0b7a31984417ee11269c7
Nicolas Garçon <nicolas.garcon@gmail.com>
2020-06-07 11:00:55 +0200

    Mapping attempt

--- back/app/resources/index.js
+++ back/app/resources/index.js
@@ -7,25 +7,49 @@ const makeId = () => Math.random().toString(36).substring(7);
 
 
 db.serialize(function () {
+
+    // USER 
+
     db.run(`CREATE TABLE users
             (
-                user_uid   VARCHAR(255) PRIMARY KEY,
+                user_uid   VARCHAR(7) PRIMARY KEY,
                 first_name VARCHAR(255) NOT NULL,
                 last_name  VARCHAR(255) NOT NULL
             );`);
 
+    //    db.run(`UPDATE TABLE "users" ADD COLUMN role VARCHAR(30) USING role = 'user';`);
+
+    db.run(`CREATE TABLE pets
+            (
+                pet_uid VARCHAR(7) PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                age  INT NOT NULL, 
+                user_uid VARCHAR(7) REFERENCES users("id")
+            );`);
 
     // Seed user
-    var stmt = db.prepare("INSERT INTO users VALUES (?,?,?)");
-    for (var i = 0; i < 10; i++) {
-        stmt.run([makeId(), faker.name.firstName(), faker.name.lastName()]);
-    }
-    stmt.finalize();
+    var stmtUsers = db.prepare("INSERT INTO users VALUES (?,?,?)");
+    var stmtInsertPets = db.prepare("INSERT INTO pets VALUES (?,?,?,?)");
 
+    for (var i = 0; i < 10; i++) {
+
+        const userId = makeId(); 
+        stmtUsers.run([userId, faker.name.firstName(), faker.name.lastName()]);
+        stmtInsertPets.run([makeId(), faker.name.firstName(), faker.random.number(50), userId]);
+    }
+
+    stmtUsers.finalize();
+    stmtInsertPets.finalize();
 
     db.each("SELECT * FROM users", function (err, row) {
         console.log(row);
-    })
+    }); 
+
+    db.each("SELECT * FROM pets", function (err, row) {
+        console.log(row);
+    });
+
+
 })
 
 
